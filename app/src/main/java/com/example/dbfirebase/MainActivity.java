0cd72e7472e3c43a8bbf0d92fec236e42a59d023
Aldi-nafi43 <aldi.nafi43@gmail.com>
2021-06-17 10:01:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dbfirebase/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dbfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button tambahbtn, lihatbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tambahbtn = findViewById(R.id.buttontambah);
+        lihatbtn = findViewById(R.id.buttonlihat);
+
+        tambahbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,TambahTeman.class);
+                startActivity(i);
+            }
+        });
+
+        lihatbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/dbfirebase/TambahTeman.java
@@ -0,0 +1,61 @@
+package com.example.dbfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.dbfirebase.database.Teman;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class TambahTeman extends AppCompatActivity {
+
+    private EditText ednama, edtelpon;
+    private Button btnsimpan;
+    private DatabaseReference database;
+    String nm, tlp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_teman);
+
+        ednama = findViewById(R.id.editnama);
+        edtelpon = findViewById(R.id.edittelpon);
+        btnsimpan = findViewById(R.id.buttonsimpan);
+
+        database = FirebaseDatabase.getInstance().getReference();
+
+        btnsimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((!ednama.getText().toString().isEmpty())&&(!edtelpon.getText().toString().isEmpty())){
+                   nm = ednama.getText().toString();
+                   tlp = edtelpon.getText().toString();
+
+                   submitData(new Teman(nm,tlp));
+
+                }else{
+                    Toast.makeText(TambahTeman.this,"data tersimpan",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    private void submitData(Teman tmn){
+        database.child("Teman").push().setValue(tmn).addOnSuccessListener(this, new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                ednama.setText("");
+                edtelpon.setText("");
+
+                Toast.makeText(TambahTeman.this,"data sukses disimpan",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
